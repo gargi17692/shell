2ce9b38e7a19f2407c0da3cf023b577812e3e29a
gargi17692 <160726145+gargi17692@users.noreply.github.com>
2024-11-25 14:16:24 +0000

    forinventory

--- inventory.sh
+++ inventory.sh
@@ -3,4 +3,7 @@
 Sever_Name=$(uname -n)
 IP_ADDRESS=$(ifconfig | grep -E "eth0|inet" | awk 'NR==3{print $2}')
 OS_TYPE=$(uname)
-UP_TIME
+UP_TIME=$(uptime | awk '{print $3}' | cut -d ',' -f 1)
+
+echo "S_No, SERVER_NAME, IP_ADDRESS, OS_TYPE, UPTIME"  > servers_info.csv
+echo "1, $Sever_Name, $IP_ADDRESS, $OS_TYPE, $UP_TIME" >> servers_info.csv
